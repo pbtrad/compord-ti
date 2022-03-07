b7bed503c0c298902726bc1dca16c8a09f96d82b
paul <paulbrowne2@gmail.com>
2022-03-07 16:28:40 +0000

    Added functionality to search bar to find name and description keywords in products

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
+from django.db.models import Q
 from django.shortcuts import render
-from product.models import Product
+from product.models import Product, Category
 
 def frontpage(request):
     products = Product.objects.all()[0:8]
@@ -8,5 +9,22 @@ def frontpage(request):
 
 def shop(request):
     products = Product.objects.all()
+    categories = Category.objects.all()
+    active_category = request.GET.get('category', '')
 
-    return render(request, 'core/shop.html', {'products': products})
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get('query', '')
+
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+
+    context = {
+        'categories': categories,
+        'products': products,
+        'active_category': active_category
+    }
+
+    return render(request, 'core/shop.html', context)
