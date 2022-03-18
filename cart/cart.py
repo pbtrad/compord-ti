ef65ac8c8b6ee49d7f92d22c2c943f1c70e67dae
paul <paulbrowne2@gmail.com>
2022-03-18 21:38:53 +0000

    Added cart menu html and htmx for add to cart button

--- cart/cart.py
+++ cart/cart.py
@@ -5,7 +5,7 @@ from product.models import Product
 class Cart(object):
     def __init__(self, request):
         self.session = request.session
-        cart = self.session.get(settings.CART_SETTINGS_ID)
+        cart = self.session.get(settings.CART_SESSION_ID)
 
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
--- compord_ti/urls.py
+++ compord_ti/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from core.views import frontpage, shop
 from product.views import product
+from cart.views import add_to_cart
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('admin/', admin.site.urls),
 ]
