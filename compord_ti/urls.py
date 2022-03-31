af1f5633bbd2196a5f4a2c268f4eb0e731975d55
paul <paulbrowne2@gmail.com>
2022-03-31 21:21:17 +0100

    Added view and url for checkout

--- compord_ti/urls.py
+++ compord_ti/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views
 from django.urls import path
 from core.views import frontpage, shop, signup
 from product.views import product
-from cart.views import add_to_cart, cart
+from cart.views import add_to_cart, cart, checkout
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
@@ -30,6 +30,7 @@ urlpatterns = [
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
     path('cart/', cart, name='cart'),
+    path('cart/checkout/', checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
