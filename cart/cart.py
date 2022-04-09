e72906ce289e1921407d2fc45f31889a34b66c53
paul <paulbrowne2@gmail.com>
2022-04-09 10:19:34 +0100

    Total cost function added to cart and cart template

--- cart/cart.py
+++ cart/cart.py
@@ -46,3 +46,9 @@ class Cart(object):
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+    def get_total_cost(self):
+        for p in self.cart.keys():
+            self.cart[str(p)]['product'] = Product.objects.get(pk=p)
+
+        return int(sum(item['product'].price * item['quantity'] for item in self.cart.values())) / 100
