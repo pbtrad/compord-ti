0838f95507f36b0f6db8b141aa39798ab0bf5408
paul <paulbrowne2@gmail.com>
2022-04-10 08:21:00 +0100

    Login required decorator added to checkout for authenticated users

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from .cart import Cart
 
@@ -9,7 +10,13 @@ def add_to_cart(request, product_id):
     return render(request, 'cart/menu_cart.html')
 
 def cart(request):
+    cart = Cart(request)
+
+    print(cart)
+    for item in cart:
+        print(item)
     return render(request, 'cart/cart.html')
 
+@login_required
 def checkout(request):
     return render(request, 'cart/checkout.html')
